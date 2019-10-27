78aeac391231b5e6951c64edf4366a7701eee5ef
ChiandEruda <g549141930@gmail.com>
2019-10-27 17:34:36 +0800

    图标切换 active 图片、slot 封装一层 div 用于绑定属性、样式、!isActive

--- src/components/tabbar/TabBarItem.vue
+++ src/components/tabbar/TabBarItem.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="tab-bar-item">
-        <slot name="item-icon"></slot>
-        <slot name="item-text"></slot>
+        <div v-if='!isActive'><slot name="item-icon"></slot></div>
+        <div v-else><slot name="item-icon-active"></slot></div>
+        <div :class='{active:isActive}'><slot name="item-text"></slot></div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'TabBarItem'
+    name: 'TabBarItem',
+    data() {
+      return {
+        isActive: false,
+      }
+    }
 }
 </script>
 
@@ -25,5 +31,9 @@ export default {
       width: 24px;
       height: 24px;
       vertical-align: middle;
+  }
+
+  .active {
+    color: #d81e06;
   }
 </style>
