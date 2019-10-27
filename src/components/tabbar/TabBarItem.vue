51263b554dc3eaf6322f8db6eb6ebff3e07ded52
ChiandEruda <g549141930@gmail.com>
2019-10-27 20:42:12 +0800

    computed 监听图标路由状态、封装图标文字颜色修改属性

--- src/components/tabbar/TabBarItem.vue
+++ src/components/tabbar/TabBarItem.vue
@@ -2,7 +2,7 @@
     <div class="tab-bar-item" @click="clickItem">
         <div v-if='!isActive'><slot name="item-icon"></slot></div>
         <div v-else><slot name="item-icon-active"></slot></div>
-        <div :class='{active:isActive}'><slot name="item-text"></slot></div>
+        <div :style="activeStyle"><slot name="item-text"></slot></div>
     </div>
 </template>
 
@@ -11,10 +11,23 @@ export default {
     name: 'TabBarItem',
     props: {
       path: String,
+      activeColor: {
+        type: String,
+        default: 'red',
+      }
     },
     data() {
       return {
-        isActive: false,
+        // isActive: false,
+      }
+    },
+    computed: {
+      isActive() {
+        console.log(this.$route.path.indexOf(this.path))
+        return !this.$route.path.indexOf(this.path)
+      },
+      activeStyle() {
+        return this.isActive? {color: this.activeColor} : {} 
       }
     },
     methods: {
